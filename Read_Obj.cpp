52ad0fe6e21eaca4454e549212f94b4aa0f86554
Catherine <cat.taylor.taylor@btinternet.com>
2017-02-23 17:45:38 +0000

    bit of face added

--- Read_Obj.cpp
+++ Read_Obj.cpp
@@ -8,8 +8,8 @@
 using namespace std;
 
 int main(){
-char str[2];
-  float f1, f2, f3;
+char str[2], c1, c2, c3, c4, c5, c6, c7, c8;
+  float f1, f2, f3, f4, f5, f6, f7, f8;
   string s = "a";
   FILE * myObject;
   int k_v = 0, k_vn=0, k_vf=0;
@@ -52,7 +52,7 @@ while (s == "vn"){
 fclose(myObject);
  myObject = fopen("cube.obj", "r");
 
-int number_of_normals = 6;
+int number_of_normals = k_vn;
  float* N = new float[3*number_of_normals];
 for (int i=0; i<12; i++){
     fscanf(myObject, "%*[^\n]\n", NULL);
@@ -64,19 +64,26 @@ for(int i=0; i<3*number_of_normals; i+=3){
      N[i+2]=f3;    
 }
 
-//  fscanf(myObject, "%s %f %f %f" , str, &f1, &f2, &f3);
-// s=str;
+for (int i=0; i<4;i++){
+ fscanf(myObject, "%s %f %f %f" , str, &f1, &f2, &f3);
+}
+s=str;
 // while(s!="f"){
 //     fscanf(myObject, "%s %f %f %f" , str, &f1, &f2, &f3);
 //     s=str;
-// cout<<"s "<<s<<"\n";
+ cout<<"s "<<s<<"\n";
 // }
-// while (s == "f"){
-//   fscanf(myObject, "%s %f %f %f" , str, &f1, &f2, &f3);
-//   s = str;
-//   k_vf=k_vf+1;
-// cout <<"str "<<str<<"f1 "<<f1<<"f2 "<<f2<<"f3 "<<f3<<"\n";
-// }
-// cout<<"k "<<k_vf<<"\n";
+
+fscanf(myObject, "%s %f %c %c %f %f %c %c %f %f %c %c %f %f %c %c %f" , str, &f1, &c1, &c2, &f2, &f3, &c3,&c4, &f4,&f5, &c5, &c6, &f6, &f7, &c7, &c8, &f8);
+  s = str;
+  cout <<"str "<<str<<"f1 "<<f1<<"f3 "<<f3<<"f5 "<<f5<<"\n";
+  cout<<"s "<<s<<"\n";
+while (s.c_str()!=EOF){
+fscanf(myObject, "%s %f %c %c %f %f %c %c %f %f %c %c %f %f %c %c %f" , str, &f1, &c1, &c2, &f2, &f3, &c3,&c4, &f4,&f5, &c5, &c6, &f6, &f7, &c7, &c8, &f8);
+  s = str;
+  k_vf=k_vf+1;
+cout <<"str "<<str<<"f1 "<<f1<<"f3 "<<f3<<"f5 "<<f5<<"\n";
+}
+cout<<"k "<<k_vf<<"\n";
     return 0;
 }
