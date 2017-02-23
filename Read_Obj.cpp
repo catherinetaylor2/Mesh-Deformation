3dc9cae2c613257eb011a21b7384b7499666e0bb
Catherine <cat.taylor.taylor@btinternet.com>
2017-02-23 21:10:35 +0000

    Began faces.

--- Read_Obj.cpp
+++ Read_Obj.cpp
@@ -8,82 +8,141 @@
 using namespace std;
 
 int main(){
-char str[2], c1, c2, c3, c4, c5, c6, c7, c8;
-  float f1, f2, f3, f4, f5, f6, f7, f8;
+char str[2], c1, c2, c3, c4, c5, c6;
+  float f1, f2, f3, f4, f5, f6, f7, f8, f9;
   string s = "a";
   FILE * myObject;
-  int k_v = 0, k_vn=0, k_vf=0;
-  //  ifstream myObject ("sphere.obj");
+  int k_v = 0, k_vn=0, k_vf=0, t;
 
-  myObject = fopen("cube.obj", "r");
+  myObject = fopen("pyramid.obj", "r");
 
-for (int i=0; i<4; i++){
-fscanf(myObject, "%*[^\n]\n", NULL);
-}
-while (s == "v"){
+//-----------------------------------------------------------------
+while (s != "v"){
+  fscanf(myObject, "%s %f %f %f" , str, &f1, &f2, &f3);
+  s = str;  
+ }
+
+ do{  
   fscanf(myObject, "%s %f %f %f" , str, &f1, &f2, &f3);
   s = str;
   k_v=k_v+1;
- }
+ }while (s != "vt");
 
 fclose(myObject);
- myObject = fopen("cube.obj", "r");
-int number_of_vertices = 8;
+ myObject = fopen("pyramid.obj", "r");
+int number_of_vertices = k_v;
  float* V = new float[3*number_of_vertices];
-for (int i=0; i<4; i++){
-fscanf(myObject, "%*[^\n]\n", NULL);
-}
-for(int i=0; i<3*number_of_vertices; i+=3){
-     fscanf(myObject, "%s %f %f %f" , str, &f1, &f2, &f3);
-   cout <<"i "<<i<<"\n";
-     V[i] = f1;
-     V[i+1] = f2;
-     V[i+2]=f3;    
-}
 
-fscanf(myObject, "%s %f %f %f" , str, &f1, &f2, &f3);
-s=str;
-while (s == "vn"){
+ while (s != "v"){
   fscanf(myObject, "%s %f %f %f" , str, &f1, &f2, &f3);
+  s = str;  
+ }
+
+ k_v = 0;
+for(int i=0; i<3*number_of_vertices; i+=3){
+  V[i] = f1;
+  V[i+1] = f2;
+  V[i+2]=f3; 
+  fscanf(myObject, "%s %f %f %f" , str, &f1, &f2, &f3);
+}
+ 
+//---------------------------------------------------------------------
+
+while (s != "vn"){
+  fscanf(myObject, "%s %f %f %f" , str, &f1, &f2, &f3);
+  s = str;  
+ }
+ cout<<"s "<<s<<"\n";
+
+ do{  
+   fscanf(myObject, "%s %f %f %f" , str, &f1, &f2, &f3);
   s = str;
   k_vn=k_vn+1;
- }
-
+ }while (s == "vn");
+cout<<"n "<<k_vn<<"\n";
 fclose(myObject);
- myObject = fopen("cube.obj", "r");
+ myObject = fopen("pyramid.obj", "r");
 
 int number_of_normals = k_vn;
  float* N = new float[3*number_of_normals];
-for (int i=0; i<12; i++){
-    fscanf(myObject, "%*[^\n]\n", NULL);
-    }
+
+ while (s != "vn"){
+  fscanf(myObject, "%s %f %f %f" , str, &f1, &f2, &f3);
+  s = str;  
+ }
 for(int i=0; i<3*number_of_normals; i+=3){
-     fscanf(myObject, "%s %f %f %f" , str, &f1, &f2, &f3);
-     N[i] = f1;
+         N[i] = f1;
      N[i+1] = f2;
-     N[i+2]=f3;    
+     N[i+2]=f3;   
+      fscanf(myObject, "%s %f %f %f" , str, &f1, &f2, &f3);
 }
+ cout<<"nx "<<N[3*number_of_normals-3]<<" ";
+  cout<<"ny "<<N[3*number_of_normals-2]<<" ";
+  cout<<"nz "<<N[3*number_of_normals-1]<<" ";
 
-for (int i=0; i<4;i++){
- fscanf(myObject, "%s %f %f %f" , str, &f1, &f2, &f3);
-}
-s=str;
-// while(s!="f"){
-//     fscanf(myObject, "%s %f %f %f" , str, &f1, &f2, &f3);
-//     s=str;
- cout<<"s "<<s<<"\n";
-// }
 
-fscanf(myObject, "%s %f %c %c %f %f %c %c %f %f %c %c %f %f %c %c %f" , str, &f1, &c1, &c2, &f2, &f3, &c3,&c4, &f4,&f5, &c5, &c6, &f6, &f7, &c7, &c8, &f8);
-  s = str;
-  cout <<"str "<<str<<"f1 "<<f1<<"f3 "<<f3<<"f5 "<<f5<<"\n";
-  cout<<"s "<<s<<"\n";
-while (s.c_str()!=EOF){
-fscanf(myObject, "%s %f %c %c %f %f %c %c %f %f %c %c %f %f %c %c %f" , str, &f1, &c1, &c2, &f2, &f3, &c3,&c4, &f4,&f5, &c5, &c6, &f6, &f7, &c7, &c8, &f8);
-  s = str;
+//--------------------------------------------------------------------
+fclose(myObject);
+ myObject = fopen("pyramid.obj", "r");
+  fscanf(myObject, "%s %f %f %f" , str, &f1, &f2, &f3);
+  s = str; 
+  while (s != "vn"){
+  fscanf(myObject, "%s %f %f %f" , str, &f1, &f2, &f3);
+  s = str;  
+ }
+ for(int i=0; i< number_of_normals-1; i++){
+   fscanf(myObject, "%s %f %f %f" , str, &f1, &f2, &f3);
+    s = str;
+    cout<<"s "<<s<<"\n";
+ }
+
+do{
+    s = str;
   k_vf=k_vf+1;
-cout <<"str "<<str<<"f1 "<<f1<<"f3 "<<f3<<"f5 "<<f5<<"\n";
-}
-cout<<"k "<<k_vf<<"\n";
+  cout<<"s "<<s<<"\n";
+  t=fscanf(myObject, "%s %f %c %f %c %f %f %c %f %c %f %f %c %f %c %f", str, &f1, &c1, &f2, &c2, &f3, &f4, &c3, &f5, &c4, &f6, &f7, &c5, &f8, &c6, &f9);
+cout<<"t "<<t<<"\n";
+cout<<"f9 "<<f9<<"\n";
+}while(t!=EOF);
+cout <<"k"<<k_vf<<"\n";
+
+fclose(myObject);
+ myObject = fopen("pyramid.obj", "r");
+ fclose(myObject);
+ myObject = fopen("pyramid.obj", "r");
+  fscanf(myObject, "%s %f %f %f" , str, &f1, &f2, &f3);
+  s = str; 
+  while (s != "vn"){
+  fscanf(myObject, "%s %f %f %f" , str, &f1, &f2, &f3);
+  s = str;  
+ }
+ for(int i=0; i< number_of_normals-1; i++){
+   fscanf(myObject, "%s %f %f %f" , str, &f1, &f2, &f3);
+    s = str;
+    cout<<"s "<<s<<"\n";
+ }
+
+int number_of_faces = k_vf-1;
+ float* FV = new float[3*number_of_faces];
+  float* FN= new float[3*number_of_faces];
+
+  for(int i=0; i<3*number_of_faces; i+=3){
+fscanf(myObject, "%s %f %c %f %c %f %f %c %f %c %f %f %c %f %c %f", str, &f1, &c1, &f2, &c2, &f3, &f4, &c3, &f5, &c4, &f6, &f7, &c5, &f8, &c6, &f9);
+FV[i] = f1;
+FV[i+1]=f4;
+FV[i+2]=f7;
+
+FN[i]=f3;
+FV[i+1] = f6;
+FV[i+2]=f9;
+
+  }
+  cout<<"FV "<<FV[3*number_of_faces -3]<<" ";
+   cout<<"FV "<<FV[3*number_of_faces -2]<<" ";
+    cout<<"FV "<<FV[3*number_of_faces -1]<<"\n";
+
+      cout<<"FN "<<FN[3*number_of_faces -3]<<" ";
+   cout<<"FN "<<FN[3*number_of_faces -2]<<" ";
+    cout<<"FN "<<FN[3*number_of_faces -1]<<"\n";
     return 0;
 }
