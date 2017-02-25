a5a4e120a52e12ce419eb8e9bbcd7857b08b0e7f
Catherine <cat.taylor.taylor@btinternet.com>
2017-02-25 12:51:40 +0000

    Used tutorial open.gl to draw triangle

--- Mesh_Deformation.cpp
+++ Mesh_Deformation.cpp
@@ -13,38 +13,94 @@
 #include <string>
 #include <cstdio>
 #include <iostream>
+#include "shader.hpp"
 
 
 int main(){
 
 glfwInit();
-
+ 
 glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
 glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", nullptr, nullptr);
+auto window = glfwCreateWindow(800, 600, "OpenGL", nullptr, nullptr);
 
-glfwMakeContextCurrent(window);
-while(!glfwWindowShouldClose(window)){
-    glfwSwapBuffers(window);
-    glfwPollEvents();
+if(window==nullptr){
+    printf("window failed \n");
+    glfwTerminate();
+    return -1;
 }
+// while(!glfwWindowShouldClose(window)){
+//     glfwSwapBuffers(window);
+//     glfwPollEvents();
+// }
 
 glewExperimental = GL_TRUE;
+glfwMakeContextCurrent(window);
 glewInit();
+
+     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+//     glClearColor(1.0f, 1.0f, 1.0f, 0.0f); //makes the screen blue.
+    
+    GLuint VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
+    
+//     // Create and compile our GLSL program from the shaders
+GLuint programID = LoadShaders( "/Users/catta/OneDrive/Documents/Course work/Computer animation and games 2/Mesh-Deformation/vertex_shader.vertexshader", "/Users/catta/OneDrive/Documents/Course work/Computer animation and games 2/Mesh-Deformation/fragment_shader.fragmentshader" );
+    
+    //C:\Users\catta\OneDrive\Documents\Course work\Computer animation and games 2\Mesh-Deformation
+
+float vertices[] = {
+     0.0f,  0.5f, 0.0f, // Vertex 1 (X, Y)
+     0.5f, -0.5f, 0.0f, // Vertex 2 (X, Y)
+    -0.5f, -0.5f, 0.0f  // Vertex 3 (X, Y)
+};
+
 
 
 GLuint vertexBuffer;
 glGenBuffers(1, &vertexBuffer);
+glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-printf("%u\n", vertexBuffer);
+do{
+        // Clear the screen
+        glClear( GL_COLOR_BUFFER_BIT );
+        
+        // Use our shader
+        glUseProgram(programID);
+        
+        // 1rst attribute buffer : vertices
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+        GLint posAttrib = glGetAttribLocation(programID, "position");
+        glEnableVertexAttribArray(posAttrib);
+       
+	    glVertexAttribPointer(0, //0 is a magic number which tells opengl what type of information it is, 0 =vertex
+								3, //size of vertex information
+								GL_FLOAT, //type of the data
+								GL_FALSE, //data is normalised
+								0, //stride
+								0 //offset, so where the data starts
+		);
+
+          glDrawArrays(GL_LINE_LOOP, 0, 3); // 3 indices starting at 0 -> 1 triangle
+        
+        glDisableVertexAttribArray(0);
+        
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+        
+    }
 
 
-if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-    glfwSetWindowShouldClose(window, GL_TRUE);
+ while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+          glfwWindowShouldClose(window) == 0 );
 
   glfwTerminate();
 
